95a56ab9689bbc1561abb1e4842497f4ae9793a2
Matheus <matheusvmja@gmail.com>
2022-08-25 18:29:05 -0300

    Layout completo

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = prompt('Qual é o seu nome?');
+
+// Criar função para pegar a variável nome e criar uma div no chat
+
+// Criar função para pegar os comentários do input e criar uma div no chat
+
+// Criar função para guardar os dados no servidor
+
+// Criar função para 
